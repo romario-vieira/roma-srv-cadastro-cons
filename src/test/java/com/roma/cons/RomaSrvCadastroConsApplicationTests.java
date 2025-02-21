6c49878cb03622e611ae0c5603c95d154c3461fc
Romário Vieira <romario.sistemas@gmail.com>
2025-02-20 23:51:52 -0300

    Iniciando o projeto
    Criando casca do projeto

---
+++ src/test/java/com/roma/cons/RomaSrvCadastroConsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.roma.cons;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RomaSrvCadastroConsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
